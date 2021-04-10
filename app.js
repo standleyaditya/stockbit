02da589a6792c11ca9a850862b502e16d11f8533
standleyaditya <standleyaditya13@gmail.com>
2021-04-10 18:31:53 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function findFirstStringInBracket(str) {
+    if (str.length > 0) {
+        let indexFirstBracketFound = str.indexOf("(");
+        if (indexFirstBracketFound >= 0) {
+            let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
+            if (wordsAfterFirstBracket) {
+                wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
+                let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+                if (indexClosingBracketFound >= 0) {
+                    return wordsAfterFirstBracket.substring(0,indexClosingBracketFound); 
+                }
+                else {
+                    return '';
+                }
+            } 
+            else {
+                return '';
+            }
+        }
+        else {
+            return '';
+        }
+    }
+    else {
+        return '';
+    }
+}
